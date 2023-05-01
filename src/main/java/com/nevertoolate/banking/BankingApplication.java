c902347173f61e0c65ea8a2d4e498d576b787001
itisnevertoolate <muhammedabdelrasoul12@gmail.com>
2023-05-02 00:07:33 +0300

    my first commit

---
+++ src/main/java/com/nevertoolate/banking/BankingApplication.java
@@ -0,0 +1,14 @@
+package com.nevertoolate.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class BankingApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/nevertoolate/banking/config/MailConfig.java
@@ -0,0 +1,30 @@
+package com.nevertoolate.banking.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+public class MailConfig {
+
+    @Bean
+    public static JavaMailSenderImpl getMailConfig(){
+        JavaMailSenderImpl emailConfig = new JavaMailSenderImpl();
+
+        // Set Properties:
+        Properties props = emailConfig.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        // Set Mail Credentials:
+        emailConfig.setHost("smtp.gmail.com");
+        emailConfig.setPort(587);
+        emailConfig.setUsername("***************");
+        emailConfig.setPassword("***************");
+
+        return emailConfig;
+    }
+    // End Of Email Config Method.
+}
